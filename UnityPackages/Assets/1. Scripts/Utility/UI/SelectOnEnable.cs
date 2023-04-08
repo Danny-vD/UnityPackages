d9bc616bbde714d69f7d91e5e156d402b94436f9
Danny-vD <dvduist@hotmail.nl>
2023-04-08 15:37:21 +0200

    Add SelectOnEnable script to Utility&Editor

---
+++ UnityPackages/Assets/1. Scripts/Utility/UI/SelectOnEnable.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using VDFramework;
+
+namespace Utility.UI
+{
+    /// <summary>
+    /// Select a specific selectable when this object is enabled
+    /// </summary>
+    public class SelectOnEnable : BetterMonoBehaviour
+    {
+        [SerializeField]
+        private Selectable selectable;
+
+        private void OnEnable()
+        {
+            if (ReferenceEquals(selectable, null))
+            {
+                return;
+            }
+            
+            selectable.Select();
+        }
+        
+        private void Start()
+        {
+            if (ReferenceEquals(selectable, null))
+            {
+                Destroy(this);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (ReferenceEquals(selectable, null))
+            {
+                return;
+            }
+        
+            if (EventSystem.current == null || EventSystem.current.alreadySelecting)
+                return;
+
+            if (ReferenceEquals(EventSystem.current.currentSelectedGameObject, selectable.gameObject))
+            {
+                EventSystem.current.SetSelectedGameObject(null);   
+            }
+        }
+    }
+}
